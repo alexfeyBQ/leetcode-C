4e97b32d9cf1b1df7f6d93d3113bcee30c7b06a3
alexfeyBQ <alexisbasquin4@gmail.com>
2025-11-01 09:10:39 +0100

    Update 231_power_of_two.c

--- easy/231_power_of_two.c
+++ easy/231_power_of_two.c
@@ -1,3 +1,8 @@
+// Problem: 231. Power of Two
+// Link: https://leetcode.com/problems/power-of-two/
+// Language: C
+// Approach: Keep dividing n by 2 until we reach 1
+
 bool isPowerOfTwo(int n)
 {
     if (n <= 0) return false;
