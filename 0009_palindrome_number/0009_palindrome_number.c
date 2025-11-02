2e5e977098ab87764b6a1ff3830a5f079f4583b6
alexfeyBQ <alexisbasquin4@gmail.com>
2025-11-02 20:49:31 +0100

    Update 0009_palindrome_number.c

--- 0009_palindrome_number/0009_palindrome_number.c
+++ 0009_palindrome_number/0009_palindrome_number.c
@@ -4,13 +4,16 @@
 
 bool isPalindrome(int x)
 {
+    // Avoid Negative numbers and numbers ending with 0 (except 0 itself)
     if (x < 0 || (x % 10 == 0 && x != 0)) return false;
 
     int reversed = 0;
 
+    // Reverse half of the number
     while (x > reversed)
     {
         reversed = reversed * 10 + x % 10, x /= 10;
     }
+    // Compare the two halves
     return x == reversed || x == reversed / 10;
 }
