c684b763c45b0186b517c6ea272ce1777439afc7
alexfeyBQ <alexisbasquin4@gmail.com>
2025-11-06 21:12:34 +0100

    Update 0001_two_sum.c

--- 0001_two_sum/0001_two_sum.c
+++ 0001_two_sum/0001_two_sum.c
@@ -9,12 +9,14 @@ int* twoSum(int* nums, int numsSize, int target, int* returnSize)
     for (int i = 0; i < numsSize; i++)
     {
         for (int j = i + 1; j < numsSize; j++)
-        if (nums[i] + nums[j] == target)
         {
-            answer[0] = i;
-            answer[1] = j;
-            *returnSize = 2;
-            return answer;
+            if (nums[i] + nums[j] == target)
+            {
+                answer[0] = i;
+                answer[1] = j;
+                *returnSize = 2;
+                return answer;
+            }
         }
     }
     return NULL;
